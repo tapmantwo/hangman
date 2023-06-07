eeaf5e1eca9d085d901e9ada24f8a9fd4d65c6e1
zakingleton <zakellisingleton@gmail.com>
2023-06-07 19:43:16 +0100

    display correct guesses

--- hangman.js
+++ hangman.js
@@ -1,10 +1,32 @@
+const word = 'cheese'
+const correctGuesses = []
+const incorrectGuesses = []
 function SetUpWord() {
-    const word = 'cheese'
     const list = document.getElementById('word')
+    DeleteChildren(list)
     for(let i = 0; i < word.length; i++) {
         const item = document.createElement('li')
+        if(correctGuesses.includes(word[i])) {
+            const value = document.createTextNode(word[i])
+            item.appendChild(value)
+        }else{
         const value = document.createTextNode('\u00A0')
         item.appendChild(value)
+        }
         list.appendChild(item)
+    }
+}
+
+function DeleteChildren(element) {
+    while(element.firstChild) {
+        element.removeChild(element.lastChild)
+    }
+}
+
+function LetterGuessed(letter) {
+    if(word.includes(letter)) {
+        correctGuesses.push(letter)
+    }else{
+        incorrectGuesses.push(letter)
     }
 }
